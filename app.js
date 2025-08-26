c8f66f2c37b65d8b94a40e3a0f75ab86272c5002
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-08-26 16:39:23 +0600

    Add requied

---
+++ app.js
@@ -0,0 +1,12 @@
+/** @format */
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+     res.send("jj");
+});
+
+app.listen(3000, () => {
+     console.log("Serving on port 3000");
+});
