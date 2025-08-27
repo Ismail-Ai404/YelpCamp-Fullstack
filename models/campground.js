e75da62da9aa8bbb86a47a4a00d1865e25ff828d
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-08-27 17:54:53 +0600

    Change the view of index add card display

--- models/campground.js
+++ models/campground.js
@@ -6,9 +6,10 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
      title: String,
-     price: String,
+     price: Number,
      description: String,
      location: String,
+     image: String,
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -22,16 +22,21 @@ const seedDB = async () => {
      await Campground.deleteMany({});
      console.log("Deleted all campgrounds");
 
+     function roundToTwoDecimalPlaces(num) {
+          return Math.round(num * 100) / 100;
+     }
      // Add 50 new campgrounds
      for (let i = 0; i < 50; i++) {
           const randomCity = sample(cities); // { city, division }
           const descriptor = sample(descriptors);
           const place = sample(places);
+          const price = roundToTwoDecimalPlaces(Math.random() * 20 + 10);
 
           const camp = new Campground({
                title: `${descriptor} ${place}`,
                location: `${randomCity.city}, ${randomCity.division}`,
-               // add other fields if necessary (price, images, description)
+               price,
+               image: "https://images.unsplash.com/photo-1641871205153-1401cab015c9?q=80&w=725&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           });
 
           await camp.save();
