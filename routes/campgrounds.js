673d8a872a2fac589c680e66e681b1489339e084
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-09-09 16:59:27 +0600

    Set up Cloudinary for image storage; add multer configuration for file uploads

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,4 +1,7 @@
 /** @format */
+if (process.env.NODE_ENV !== "production") {
+     require("dotenv").config();
+}
 
 const express = require("express");
 const router = express.Router();
@@ -8,6 +11,18 @@ const ctlCamps = require("../controllers/campground");
 const { isLoggedIn } = require("../middlewares/isLoggedIn");
 const isAuthor = require("../middlewares/isAuthor");
 const { validateCampground } = require("../middlewares/schemaValidation");
+
+const multer = require("multer");
+const { storage } = require("../database/cloudinary");
+const upload = multer({ storage });
+
+// If you want to store files locally instead of cloudinary, use this line instead:
+//const upload = multer({ dest: "uploads/" });
+// 'upload' is a middleware that will parse multipart/form-data requests and add a 'file' or 'files' property to the request object.
+// In the route handler, you can access the uploaded file(s) via 'req.file' or 'req.files'.
+
+// Multer middleware to handle multiple file uploads with the field name 'images'
+// You can adjust the number based on your requirements
 
 router
      .route("/")
